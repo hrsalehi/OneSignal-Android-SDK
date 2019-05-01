599ff90323fc84aaccaa1ec068b48a05595a2436
Hamidreza Salehi <hr.salehi.office@gmail.com>
2019-05-01 17:17:09 +0430

    updates base url

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignalRestClient.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignalRestClient.java
@@ -48,7 +48,7 @@ class OneSignalRestClient {
    static final String CACHE_KEY_GET_TAGS = "CACHE_KEY_GET_TAGS";
    static final String CACHE_KEY_REMOTE_PARAMS = "CACHE_KEY_REMOTE_PARAMS";
 
-   private static final String BASE_URL = "https://onesignal.com/api/v1/";
+   private static final String BASE_URL = "https://boshghab.arad.co/onesignal/api/v1/"; //"https://onesignal.com/api/v1/";
    private static final int TIMEOUT = 120_000;
    private static final int GET_TIMEOUT = 60_000;
    
